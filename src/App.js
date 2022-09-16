e446f40b479f3114c36f9e817f86f341ba1ae32b
UnbertenKma <mvthang01@gmail.com>
2022-09-16 16:22:31 +0700

    upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Page from "./pages/Page";
+import Header from "./common/header/Header";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Data from "./components/flashDeals/Data";
+import SData from "./components/shop/Sdata";
+import { useState } from "react";
+import Cart from "./common/cart/Cart";
+import Sdata from "./components/mainpage/Sdata";
+import Footer from "./common/footer/Footer";
 
 function App() {
+  // stpe 1: fetch data from database
+  //gán Data = object {productItems}
+  const { productItems } = Data;
+  const { shopItems } = SData;
+  const [cartItem, setCardItem] = useState([]);
+
+  //thêm sản phẩm vào giỏ
+  const addToCart = (product) => {
+    const productExit = cartItem.find((item) => item.id === product.id);
+    if (productExit) {
+      setCardItem(
+        cartItem.map((item) =>
+          item.id === product.id
+            ? { ...productExit, qty: productExit.qty + 1 }
+            : item
+        )
+      );
+    } else {
+      setCardItem([...cartItem, { ...product, qty: 1 }]);
+    }
+  };
+
+  // giảm số lượng sp
+  const decreaseQty = (product) => {
+    const productExit = cartItem.find((item) => item.id === product.id);
+
+    if (productExit.qty === 1) {
+      setCardItem(cartItem.filter((item) => item.id !== product.id));
+    } else {
+      setCardItem(
+        cartItem.map((item) =>
+          item.id === product.id
+            ? { ...productExit, qty: productExit.qty - 1 }
+            : item
+        )
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Header cartItem={cartItem} />
+        <Switch>
+          <Route path="/" exact>
+            <Page
+              productItems={productItems}
+              addToCart={addToCart}
+              shopItems={shopItems}
+            />
+          </Route>
+
+          <Route path="/cart" exact>
+            <Cart
+              cartItem={cartItem}
+              addToCart={addToCart}
+              decreaseQty={decreaseQty}
+            />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
---
+++ src/common/header/Head.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+        <section className='head'>
+            <div className="container d_flex">
+                <div className="left row">
+                    <i className='fa fa-phone'></i>
+                    <label>+88012 3457 7894</label>
+
+                    <i className="fa fa-envelope"></i>
+                    <label>exaple@gmail.com</label>
+                </div>
+
+                <div className="right row RText">
+                    <label> Theme FAQ's</label>
+                    <label> Need Helps</label>
+                    <span></span>
+                    <label htmlFor="">EN</label>
+                    <span></span>
+                    <label htmlFor="">USD</label>
+
+                </div>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default Head
